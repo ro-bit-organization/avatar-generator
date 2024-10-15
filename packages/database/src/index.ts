8febc9f1f4024393fe19db902da00b8319e048cb
ro-bit-admin <admin+github@ro-bit.ro>
2024-10-15 12:35:00 +0300

    Repository initialization

---
+++ packages/database/src/index.ts
@@ -0,0 +1,13 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+
+const neon = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+const adapter = new PrismaNeon(neon);
+
+export const client = new PrismaClient({ adapter });
+
+export * from '@prisma/client';
